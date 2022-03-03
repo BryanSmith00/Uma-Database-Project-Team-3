d283962502ed707e748d3b10d16776a4e27262b8
Bryan <bryansmith8023@gmail.com>
2022-03-03 15:12:06 -0600

    Updated create commands

--- mysql scripts/create_tables.sql
+++ mysql scripts/create_tables.sql
@@ -5,4 +5,11 @@ song_id int PRIMARY KEY,
 song_name char(32),
 length int unsigned,
 number_of_plays int unsigned
-)
+);
+
+CREATE TABLE album (
+	album_id int PRIMARY KEY,
+    album_name char(32),
+    date_created date
+);
+

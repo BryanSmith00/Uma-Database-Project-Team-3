81603e2fc3ee2b369c2a9dd37369f9d552e721e0
Bryan <bryansmith8023@gmail.com>
2022-03-31 14:30:56 -0500

    Added more get

--- CoogMusic/app.js
+++ CoogMusic/app.js
@@ -2,19 +2,25 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.static(__dirname));
 
 app.get('/', function(request, response) {
-    app.use(express.static(__dirname));
     response.statusCode = 200;
-    response.sendFile(__dirname + "/index.html");
+    response.sendFile(__dirname + "/views/index.html");
+});
+
+app.get('/login', function(request, response) {
+    response.statusCode = 200;
+    response.sendFile(__dirname + "/views/login.html");
 });
 
 app.post("/", (request, response) => {
     res.send("Thank you for subscribing");
 });
 
+
+
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`);
     console.log(__dirname);
-    console.log(__dirname + '\\styles');
 });

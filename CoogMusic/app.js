9f50c679caa3f71db8386d16bb03775717197e58
Bryan <bryansmith8023@gmail.com>
2022-03-31 15:13:07 -0500

    trash

--- CoogMusic/app.js
+++ CoogMusic/app.js
@@ -1,23 +1,67 @@
 const express = require('express');
+var http = require('http');
+const bp = require('body-parser')
 const app = express();
 const port = 3000;
 
 app.use(express.static(__dirname));
+app.use(bp.json())
+app.use(bp.urlencoded({ extended: true }))
 
 app.get('/', function(request, response) {
     response.statusCode = 200;
-    response.sendFile(__dirname + "/views/index.html");
+    //response.sendFile(__dirname + "/views/index.html");
+    response.sendFile(__dirname + "/public/php/test.html");
 });
 
 app.get('/login', function(request, response) {
     response.statusCode = 200;
+    console.log(request.body.data)
     response.sendFile(__dirname + "/views/login.html");
 });
 
-app.post("/", (request, response) => {
-    res.send("Thank you for subscribing");
+app.post("/login", (request, response) => {
+    console.log(request.body.data)
+    //response.sendFile(__dirname + "/public/php/test.html")
+    postToPHP(request.body.data);
 });
 
+function postToPHP (data) {
+
+    var options = {
+        host : 'localhost',
+        port : 3000,
+        path : '/CoogMusic/views.login.php',
+        method : 'POST',
+        headers : {
+            'Content-Type' : 'application/json',
+            'Content-Length' : Buffer.byteLength(data)
+        }
+    };
+
+    var buffer = "";
+
+    var reqPost = http.request(options, function(res) {
+        console.log("statusCode: ", res.statusCode);
+
+        res.on('data', function(d) {
+            console.info('POST Result:\n');
+            buffer = buffer+data;
+            console.info('\n\nPOST completed');
+
+        });
+
+        res.on('end', function() {
+            console.log(buffer);
+        });
+    });
+
+    console.log("before write: "+data);
+
+    reqPost.write(data);
+    reqPost.end();
+
+}
 
 
 app.listen(port, () => {
--- CoogMusic/login.php
+++ CoogMusic/login.php
@@ -1,10 +1,10 @@
 <?php
 $servername = "team-3-3380.cbbxip0p57sn.us-east-2.rds.amazonaws.com";
 $username = "admin";
-$password = "*ZrYDzw*Puctowy$nf42";
+$password = "*ZrYDzw*Puctowy\$nf42";
 
 // Create connection
-$conn = new mysqli($team-3-3380.cbbxip0p57sn.us-east-2.rds.amazonaws.com, $admin, $*ZrYDzw*Puctowy$nf42);
+$conn = new mysqli($servername, $username, $password);
 
 // Check connection
 if ($conn->connect_error) {

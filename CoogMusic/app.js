142309e006f81ec07453e6d69b94dd1c70845c41
smleake <sethleake@hotmail.com>
2022-03-31 18:50:08 -0500

    added media player

--- CoogMusic/app.js
+++ CoogMusic/app.js
@@ -6,9 +6,11 @@ const port = 3000;
 app.get('/', function(request, response) {
     app.use(express.static(__dirname));
     response.statusCode = 200;
-    response.sendFile(__dirname + "/index.html");
+    response.sendFile(__dirname + "/views/index.html");
 });
-
+app.get('/login', (req, res) => {
+    res.sendFile(__dirname + "/views/login.html")
+})
 app.post("/", (request, response) => {
     res.send("Thank you for subscribing");
 });
@@ -16,5 +18,5 @@ app.post("/", (request, response) => {
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`);
     console.log(__dirname);
-    console.log(__dirname + '\\styles');
+    // console.log(__dirname + '\\styles');
 });

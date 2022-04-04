06c5922c6c8577cc2b5d8d1e3e3aa6593321d263
Bryan <bryansmith8023@gmail.com>
2022-04-04 17:16:41 -0500

    temp

--- CoogMusic/app.js
+++ CoogMusic/app.js
@@ -1,60 +1,84 @@
 const express = require('express');
-const cookieParser = require("cookie-parser");
-const sessions = require('express-session');
-var http = require('http');
+const session = require('express-session');
 const bp = require('body-parser')
-var mysql = require('mysql');
-const app = express();
+var passport = require('passport');
+const connection = require('./public/js/database');
+
+var app = express();
 const port = 3000;
 
 app.use(express.static(__dirname));
 app.use(bp.json())
 app.use(bp.urlencoded({ extended: false }))
- 
 
-//--------------HTTP GET functions--------------//
+//---------------Session stup---------------//
 
-//Landing page GET
+app.use(session({
+    secret: "tempsecret",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        maxAge: 1000 * 365 * 60 * 60 * 24 // 1 year
+    }
+}));
+
+
+//---------------Passport---------------//
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use((req, res, next) => {
+    console.log(req.session);
+    console.log(req.user);
+    next();
+});
+    
+//--------------Routes--------------//
+//GET
+
+//Landing page route
 app.get('/', function(req, res, next) {
     res.statusCode = 200;
     res.sendFile(__dirname + "/views/index.html");
 });
 
-//Login page GET
+//Login page route
 app.get('/login', function(req, res, next) {
     res.statusCode = 200;
     res.sendFile(__dirname + "/views/login.html");
 });
 
-//Music player GET
+//Sign up page route
+app.get('/signup', function(req, res, next) {
+    res.statusCode = 200;
+    res.sendFile(__dirname + "/views/signup.html");
+});
+
+//Music player route
 app.get('/music', (req, res, next) =>{
-    res.sendFile(__dirname + "/views/music-player.html")
+    res.sendFile(__dirname + "/views/music-player.html");
+})
+
+//Good login route
+app.get('/login-success', (req, res, next) =>{
+    res.sendFile(__dirname + "");
+})
+
+//Failed login route
+app.get('/login-fail', (req, res, next) =>{
+    res.send("Your username or password was incorrect");
 })
 
 
-//--------------HTTP POST functions--------------//
+//POST
 
-//Login page POST
+//Login page route
 app.post("/login", (req, res, next) => {
     console.log(req.body)
     let username = req.body.username;
     let password = req.body.password;
     //res.send(`Username: ${username} Password: ${password}`);
-
-    //Establishes the connection to the mysql server
-    var connection = mysql.createConnection({
-        host: "team-3-3380.cbbxip0p57sn.us-east-2.rds.amazonaws.com",
-        user: "admin",
-        password: "*ZrYDzw*Puctowy\$nf42",
-        database: "coog_music"
-      });
-    
-    connection.connect(function(err) {
-        if (err) throw err;
-        else
-            console.log("successful connection");
-
-      });
     
     connection.query("SELECT * FROM user WHERE username= ? AND pass = ?", [username, password], function (error, results, fields) {
         if (error) throw error;
@@ -67,9 +91,10 @@ app.post("/login", (req, res, next) => {
       });
 });
 
+
+//--------------Server--------------//
+
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`);
     console.log(__dirname);
-});
-
-
+}); 

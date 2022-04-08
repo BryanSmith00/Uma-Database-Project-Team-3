fe0384e86cfaac426f92153d9fc1e978cc164b37
deamran <82482255+deamran@users.noreply.github.com>
2022-04-08 13:00:45 -0500

    updated sql scripts

---
+++ mysql scripts/tracksToInsert.sql
@@ -0,0 +1,119 @@
+INSERT INTO track (
+song_file,
+song_name,
+length,
+published_by)
+VALUES (
+'https://www.bensound.com/bensound-music/bensound-ukulele.mp3',
+'Ukulele',
+2.26,
+'electricblueowl'
+);
+
+INSERT INTO track (
+song_file,
+song_name,
+length,
+published_by)
+VALUES (
+'https://www.bensound.com/bensound-music/bensound-epic.mp3',
+'Epic',
+2.58,
+'cornsilkterrier'
+);
+
+INSERT INTO track (
+song_file,
+song_name,
+length,
+published_by)
+VALUES (
+'https://www.bensound.com/bensound-music/bensound-slowmotion.mp3',
+'Slow Motion',
+3.26,
+'denimlion'
+);
+
+INSERT INTO track (
+song_file,
+song_name,
+length,
+published_by)
+VALUES (
+'https://www.bensound.com/bensound-music/bensound-scifi.mp3',
+'Sci-Fi',
+4.44,
+'alabasterraven'
+);
+
+INSERT INTO track (
+song_file,
+song_name,
+length,
+published_by)
+VALUES (
+'https://www.bensound.com/bensound-music/bensound-dreams.mp3',
+'Dreams',
+3.30,
+'aquamarineelephant'
+);
+
+INSERT INTO track (
+song_file,
+song_name,
+length,
+published_by)
+VALUES (
+'https://www.bensound.com/bensound-music/bensound-sweet.mp3',
+'Sweet',
+5.07,
+'denimlion'
+);
+
+INSERT INTO track (
+song_file,
+song_name,
+length,
+published_by)
+VALUES (
+'https://www.bensound.com/bensound-music/bensound-newdawn.mp3',
+'New Dawn',
+5.13,
+'firebrickfalcon'
+);
+
+INSERT INTO track (
+song_file,
+song_name,
+length,
+published_by)
+VALUES (
+'https://www.bensound.com/bensound-music/bensound-groovyhiphop.mp3',
+'Groovy Hip-Hop',
+4.00,
+'electricblueowl'
+);
+
+INSERT INTO track (
+song_file,
+song_name,
+length,
+published_by)
+VALUES (
+'https://www.bensound.com/bensound-music/bensound-highoctane.mp3',
+'High Octane',
+2.35,
+'sandybrownbuzzard'
+);
+
+INSERT INTO track (
+song_file,
+song_name,
+length,
+published_by)
+VALUES (
+'https://www.bensound.com/bensound-music/bensound-pianomoment.mp3',
+'Piano Moment',
+1.50,
+'aquamarineelephant'
+);

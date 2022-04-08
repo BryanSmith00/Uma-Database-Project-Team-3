33ae662eb10c66d590615b8ffd4c7dae389e4d76
Gleici Pereira <gpcaresdev@gmail.com>
2022-04-08 01:43:40 -0500

    admin, table menu, initial modals

--- CoogMusic/public/js/users.js
+++ CoogMusic/public/js/users.js
@@ -1,15 +1,1 @@
-var express = require('express');
-var userQuery = express.userQuery();
-var db=require('./database');
-
-//fetch users from MySQL database
-userQuery.get('/user', function(req, res, next) {
-    var sql='SELECT user_id, username, date_created * FROM user';
-    db.query(sql, function (err, data, fields) {
-    if (err) throw err;
-    res.render('user', { title: 'Users', userData: data});
-  });
-});
-
-module.exports = userQuery;
 
